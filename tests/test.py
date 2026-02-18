5b419c63de514f83ed857fa1b41e45d7c6fc4c2e
Alex Fox <alextsfox@gmail.com>
2026-02-17 17:02:33 -0700

    new numpy method is magnitudes faster

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,26 @@
 from pathlib import Path
 import sys
+from time import perf_counter as timer
 print(Path(__file__).parent.parent)
 sys.path.append(str(Path(__file__).parent.parent))
 from camp2ascii.mymain import main
+from camp2ascii import camp2ascii as c2a
 if __name__ == "__main__":
-    path = Path("tests/tob3/23313_Site4_300Sec5.dat")
+    path = Path("/home/alextsfox/git-repos/camp2ascii/tests/tob3/2991.CPk_BBUF3m_10Hz620.dat")
     # with open("../tests/tob3/23313_Site4_300Sec5_manually_corrupted.dat", "wb") as fout:
     #     fout.write(input_buff.read(30_000))
     # path = Path("../tests/tob3/23313_Site4_300Sec5_manually_corrupted.dat")
 
-    main(path)
+    t0 = timer()
+    # main(path)
+    main(path).iloc[::600].plot(subplots=True, figsize=(15, 5))
+    import matplotlib.pyplot as plt
+    plt.show()
+    t1 = timer()
+    print(f"Total execution time (new method): {t1-t0:.2f} seconds")
+
+
+    t0 = timer()
+    c2a(path, path.parent.parent / "toa5-c2a/output_old_method.csv", pbar=False)
+    t1 = timer()
+    print(f"Total execution time (old method): {t1-t0:.2f} seconds")
